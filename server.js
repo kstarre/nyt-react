1225d8077dd069261dacb8c2e535c07dc2907a2a
Katie Starrett <starrett.k.e@gmail.com>
2017-09-13 16:33:55 -0400

    add mongoose connection to server.js

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-console.log("Hello world");
+// Include Server Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+// Require models
+
+
+// 
+const PORT = process.env.PORT || 3000;
+let app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("./public"));
+
+// Mongoose connection
+mongoose.connect("mongodb://localhost/nyt-react", { useMongoClient: true });
+
+// Routes
+
+
+
+
+// Start express server
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
