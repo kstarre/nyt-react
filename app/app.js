44fdba376ce91dfbb2b8ae1c8e547c965f2f0abd
Katie Starrett <starrett.k.e@gmail.com>
2017-09-13 15:25:34 -0400

    initial file set-up

---
+++ app/app.js
@@ -0,0 +1,8 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+// Include the main component
+//var Parent = require("./components/Parent");
+
+// Render main component
+//ReactDOM.render(<Parent />, document.getElementById("app"));
